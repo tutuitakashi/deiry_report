f1bde27b61597efebd1e7a96200e76238fa78614
tutui.takashi <porigonnz1722@gmail.com>
2021-06-03 23:10:26 +0900

    Add Employee update

--- src/controllers/employees/EmployeesEditServlet.java
+++ src/controllers/employees/EmployeesEditServlet.java
@@ -16,7 +16,7 @@ import utils.DBUtil;
 /**
  * Servlet implementation class EmployeesEditServlet
  */
-@WebServlet("/EmployeesEditServlet")
+@WebServlet("/employees/edit")
 public class EmployeesEditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
